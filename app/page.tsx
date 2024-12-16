07a7f7147b87f381fd0e78ec88a516bf8e0833aa
cnottingham1990 <nottingham.cody@gmail.com>
2024-12-15 22:28:51 -0500

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import TaskPlanner from '@/components/task-planner'
-
-export default function Home() {
-  return (
-    <main className="min-h-screen p-4">
-      <TaskPlanner />
-    </main>
-  )
-}
